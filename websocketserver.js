7f5e521ffb80a84890f4c70d650989fd3f41f60b
CunningLearner <sahdev.himan@gmail.com>
2017-08-06 21:14:16 +0530

    Add files via upload

---
+++ websocketserver.js
@@ -0,0 +1,19 @@
+var WebSocketServer = require('ws').Server;
+
+var PORT = 5000;
+
+var wss = new WebSocketServer({port: PORT});
+
+var messages = [];
+wss.on('connection', function (ws) {
+  messages.forEach(function(message){
+    ws.send(message);
+  });
+  ws.on('message', function (message) {
+    messages.push(message);
+    console.log('Message Received: %s', message);
+    wss.clients.forEach(function (conn) {
+      conn.send(message);
+    });
+  });
+});
